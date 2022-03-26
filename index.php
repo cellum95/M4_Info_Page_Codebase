b54da7ad6d88d15d5802828b2597f805486193a6
cellum95 <101460624+cellum95@users.noreply.github.com>
2022-03-26 22:06:48 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset = "utf-8">
+    <meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    <title>Personal Information</title>
+    <meta name = "description" content = "A webpage showing personal information from database">
+	<link href = "CSS/style.php" rel = "stylesheet" type = "text/css">
+
+</head>
+
+<body>
+
+	<header class = "header-title">
+		<h1>Personal Information</h1>
+	</header>
+	
+	<nav class = "nav-divider">
+		<br>
+	</nav>
+	
+	<br>
+
+	<nav class = "navbar">
+
+		<ul class = "navbar-nav">
+
+			<li class = "nav-item">
+				<a href = "">Logout</a>
+			</li>
+
+		</ul>
+
+	</nav>
+	
+	<div class = "div-container">
+
+
+		<div class = "div-child one">
+			<h2>My Information</h2><br>
+			<h3>Welcome </h3>
+			<h3>User Level: </h3>
+			<h3>Birthday: </h3>
+			<h3>Contact Details:</h3>
+			<h3 style = "margin-left: 40px;">Contact: </h3>
+			<h3 style = "margin-left: 40px;">Email: </h3>
+		</div>
+
+		<div class = "div-chile two">
+			<img src = "">
+		</div>
+		
+	</div>
+
+	<nav class = "navbar">
+
+		<ul class = "navbar-nav">
+
+			<li class = "nav-item">
+				<a href = "">Upload Image</a>
+			</li>
+
+			<li class = "nav-item">
+				<a href = "" style = "margin-left: 40px;">Reset My Password</a>
+			</li>
+
+		</ul>
+
+	</nav>
+
+</body>
+
+</html>
